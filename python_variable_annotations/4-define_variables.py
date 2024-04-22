23cb18bc50f0269bdbb0e92be4ce4fc111d41ede
Mottais <mottais.afy@outlook.fr>
2024-04-22 12:28:21 +0200

    Task4 : ajout du typage

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Définit des variables dont le type est explicite"""
+"""Definition et typage de variables"""
 
 
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = 'Holberton'
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
