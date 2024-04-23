2887bd122979bb4056f3556256d3193484653163
Mottais <mottais.afy@outlook.fr>
2024-04-23 14:41:13 +0200

    Ajout typing

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,10 +2,12 @@
 """"Contient méthode asynchrone task_wait_n"""
 
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """lance n tâches asynchrones 'wait_random' qui s'exécutent en parallèle
     et durent au maximum max_delay secondes"""
 
