fdfb13fa0f4df0385d24b31fb531013f79be7714
Mottais <mottais.afy@outlook.fr>
2024-04-23 14:42:30 +0200

    Correction pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,7 +6,6 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """lance n tâches asynchrones 'wait_random' qui s'exécutent en parallèle
     et durent au maximum max_delay secondes"""
