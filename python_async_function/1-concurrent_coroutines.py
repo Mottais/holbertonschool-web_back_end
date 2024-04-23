bd4278a66d9e1b59185183b7b76b15c6c65e24b6
Mottais <mottais.afy@outlook.fr>
2024-04-23 12:51:12 +0200

    test

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def wait_n(n, max_delay):
     """lance n tâches asynchrones 'wait_random' qui s'exécutent en parallèle
     et dure au maximum max_delay secondes"""
 
