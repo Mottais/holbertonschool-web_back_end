7c1d71472c0c1a01dcce727ba461112630375be0
Mottais <mottais.afy@outlook.fr>
2024-04-23 13:19:05 +0200

    Suppr typage et import pas utilisé et ajustement des commentaires

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""measure runtime"""
+"""contient la fonction measure_time"""
 import asyncio
-import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    """measure time / n"""
+def measure_time(n, max_delay):
+    """retourne le temps d'execution de la fonction wait_n
+    qui est le temps d'execution de la coroutine la plus longue"""
     delais = asyncio.run(wait_n(n, max_delay))
     return (max(delais))
