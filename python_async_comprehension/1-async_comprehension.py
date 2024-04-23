6e963ad317c4e56d09c47313f63d6c9e9864b97e
Mottais <mottais.afy@outlook.fr>
2024-04-23 16:47:04 +0200

    Autre solution

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,9 +6,13 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Crée liste contenant 10 nbs aléatoires."""
-    Liste_10_nb_aleatoires = []
+
+    '''Liste_10_nb_aleatoires = []
 
     async for yield_value in async_generator():
         Liste_10_nb_aleatoires.append(yield_value)
 
-    return Liste_10_nb_aleatoires
+    return Liste_10_nb_aleatoires'''
+
+    # Autre méthode
+    return [yield_value async for yield_value in async_generator()]
