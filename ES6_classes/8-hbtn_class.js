d4f756afc1a385ee4b797d90f635209922f17c56
Mottais <mottais.afy@outlook.fr>
2024-05-07 19:51:32 +0200

    Suppression commentaire

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -8,4 +8,3 @@ export default class HolbertonClass {
 
   toString() { return this._location; }
 }
-// Lorsque vous appelez la fonction Number() sur un objet, JavaScript recherche une méthode valueOf() dans cet objet pour obtenir sa représentation numérique. Si la méthode valueOf() est présente dans l'objet, elle est utilisée pour effectuer la conversion en nombre. Si la méthode valueOf() n'est pas définie ou si elle ne renvoie pas un nombre, JavaScript utilise ensuite la méthode toString() pour obtenir une représentation sous forme de chaîne de caractères, puis tente de la convertir en nombre.
