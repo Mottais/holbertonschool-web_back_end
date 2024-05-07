4a1397a47b35ca7df0584d139c347c21f139d486
Mottais <mottais.afy@outlook.fr>
2024-05-07 19:19:18 +0200

    Task6

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') { throw new Error('Class extending Building must override evacuationWarningMessage'); }
     this.sqft = sqft;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
+  get sqft() {return this._sqft;}
 
-  set sqft(value) { this._sqft = value; }
+  set sqft(newValue) {this._sqft = newValue;}
 }
