f470d61585c25a2dea909db1e443c75136f09893
Mottais <mottais.afy@outlook.fr>
2024-05-07 16:42:12 +0200

    Syntaxe plus consise

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,21 +4,14 @@ export default class Currency {
     this.name = name;
   }
 
-  get name() {
-    return this._name;
+  get name() {return this._name;}
+
+  set name(name) {this._name = name;
   }
 
-  set name(name) {
-    this._name = name;
-  }
+  get code() {return this._code;}
 
-  get code() {
-    return this._code;
-  }
-
-  set code(code) {
-    this._code = code;
-  }
+  set code(code) {this._code = code;}
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
