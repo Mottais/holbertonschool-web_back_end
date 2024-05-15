3db3cbe4fe9c060492f79d1e674c284f09b978d7
Mottais <mottais.afy@outlook.fr>
2024-05-15 14:40:50 +0200

    Ajout commentaire

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -28,3 +28,8 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
 
   return output;
 }
+
+
+// Ci-dessous une solution plus simple mais qui ne passe pas le checker
+// C'est sans doute encore un pb du checker...
+// return Promise.allSettled([signUpPromise, uploadPhotoPromise]);
