2e312fd79bfc8ee5ce3e2e5233a82546015e1c19
Mottais <mottais.afy@outlook.fr>
2024-05-15 12:08:19 +0200

    correction syntaxe suivant ESlint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,21 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup (firstName, lastName, fileName) {
-  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([signUpUser(firstName, lastName),
+    uploadPhoto(fileName)]);
 
   const output = [];
-  for (let i = 0; i < results.length; i++) {
+  for (let i = 0; i < results.length; i = 1 + 1) {
     const result = results[i];
     if (result.status === 'fulfilled') {
       output.push({
         status: result.status,
-        value: result.value
+        value: result.value,
       });
     } else {
       output.push({
         status: result.status,
-        value: `Error: ${result.reason.message}`
+        value: `Error: ${result.reason.message}`,
       });
     }
   }
