b6c4a3e327f30f46e9a4ceb2f158f1929c824844
Mottais <mottais.afy@outlook.fr>
2024-05-15 12:22:39 +0200

    Ajout commentaire

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
-
+  // Le résultat de Promise.allSettled est un tableau d'objets où chaque objet contient le
+  // statut (status) et la valeur ou la raison du rejet (value ou reason) de chaque promesse.
+  // contrairement à Promise. all qui renvoi une seule promesse qui est rejetée si une des promesses
+  // est rejetée.
   const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
 
   const output = [];
