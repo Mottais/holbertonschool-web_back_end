d1807ae8f8753817d6ab192ceacac91ff1d004fd
Mottais <mottais.afy@outlook.fr>
2024-05-20 16:46:53 +0200

    fix esint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  set.forEach(value => {
+  set.forEach((value) => {
     if (value.startsWith(startString)) {
       result.push(value.substring(startString.length));
     }
