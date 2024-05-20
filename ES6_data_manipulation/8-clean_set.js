aa80d3801a612346d33d823be03e19573212804a
Mottais <mottais.afy@outlook.fr>
2024-05-20 16:51:22 +0200

    gestion autres cas particulier de startString

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') { return ''; }
+  if (!startString || typeof startString !== 'string') return '';
   const result = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
